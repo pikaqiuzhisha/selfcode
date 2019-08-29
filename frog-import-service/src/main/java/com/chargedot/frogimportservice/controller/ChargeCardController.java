3022d7b3f084c7fdf50789f33ad8561697a47a50
chenqianlong <chenqianlong@chargefrog.com>
2019-08-29 11:17:37 +0800

    frog-import-service多环境配置文件修改

--- frog-import-service/src/main/java/com/chargedot/frogimportservice/controller/ChargeCardController.java
+++ frog-import-service/src/main/java/com/chargedot/frogimportservice/controller/ChargeCardController.java
@@ -6,13 +6,10 @@ import com.chargedot.frogimportservice.model.ChargeCard;
 import com.chargedot.frogimportservice.service.ChargeCardService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -24,16 +21,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/card")
 public class ChargeCardController {
-
-    //创建日志对象
-    private static final Logger log = LoggerFactory.getLogger(ChargeCardController.class);
-
     //创建卡管理业务对象
     @Autowired
     private ChargeCardService chargeCardService;
-
-    @Autowired
-    private KafkaTemplate kafkaTemplate;
 
     @HystrixCommand(fallbackMethod = "defaultSendMessage")
     @RequestMapping(value = "/card_import", method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
