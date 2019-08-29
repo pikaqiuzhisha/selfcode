ddda9547d72e699e3dcf67e5bb17f9b90c4b368b
mingming.gui <ahqsgmm@163.com>
2019-08-29 11:37:57 +0800

    服务类名调整

--- frog-charge-service/src/main/java/com/chargedot/charge/FrogChargeServiceApplication.java
+++ frog-charge-service/src/main/java/com/chargedot/charge/FrogChargeServiceApplication.java
@@ -3,15 +3,14 @@ package com.chargedot.charge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableTransactionManagement
-public class ElecFrogServiceApplication {
+public class FrogChargeServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ElecFrogServiceApplication.class, args);
+		SpringApplication.run(FrogChargeServiceApplication.class, args);
 	}
 }
--- frog-gateway-service/src/main/java/com/chargedot/gateway/FrogGatewayServiceApplication.java
+++ frog-gateway-service/src/main/java/com/chargedot/gateway/FrogGatewayServiceApplication.java
@@ -10,9 +10,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
-public class ChargeGatewayApplication {
+public class FrogGatewayServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChargeGatewayApplication.class, args);
+        SpringApplication.run(FrogGatewayServiceApplication.class, args);
     }
 }
